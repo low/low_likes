d0ef5c94636eb5e941f10289c336464017e77090
Lodewijk Schutte <lodewijk@gmail.com>
2012-10-03 12:07:09 +0200

    gitignore and clean config

--- low_likes/config.php
+++ low_likes/config.php
@@ -4,8 +4,7 @@
  * Low Likes config file
  *
  * @package        low_likes
- * @author         Lodewijk Schutte <hi@gotolow.com>
- * @link           http://gotolow.com/addons/low-likes
+ * @author         Lodewijk Schutte
  * @copyright      Copyright (c) 2012, Low
  */
 
@@ -14,9 +13,5 @@ if ( ! defined('LOW_LIKES_NAME'))
 	define('LOW_LIKES_NAME',    'Low Likes');
 	define('LOW_LIKES_PACKAGE', 'low_likes');
 	define('LOW_LIKES_VERSION', '0.0.1');
-	define('LOW_LIKES_DOCS',    'http://gotolow.com/addons/low-likes');
+	define('LOW_LIKES_DOCS',    'http://gotolow.com/');
 }
-
-// $config['name']    = LOW_LIKES_NAME;
-// $config['version'] = LOW_LIKES_VERSION;
-// $config['nsm_addon_updater']['versions_xml'] = LOW_LIKES_DOCS.'/feed';
